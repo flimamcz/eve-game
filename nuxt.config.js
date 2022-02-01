3106deca91a7d44455794ac0eed94163c3b61947
Filipe <cimofelipe4@gmail.com>
2022-02-01 13:26:45 -0300

    altered nextConfig favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'eve-history',
+    title: 'EveHistory',
     htmlAttrs: {
       lang: 'en'
     },
